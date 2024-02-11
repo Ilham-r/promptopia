8e1855cd5fe1c27b01ee319004bab92f15309dbe
Ilham-r <66206508+Ilham-r@users.noreply.github.com>
2024-02-11 22:33:48 +0100

    fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,61 +1,54 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
-
 const EditPrompt = () => {
   const [submitting, setSubmitting] = useState(false);
   const router = useRouter();
-  const { id } = router.query;
+  const SearchParams = useSearchParams();
+  const promptId = SearchParams.get("id");
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
-
   useEffect(() => {
     const getPromptDetails = async () => {
-      try {
-        const response = await fetch(`/api/prompt/${id}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch prompt details");
-        }
-        const data = await response.json();
-        setPost({
-          prompt: data.prompt,
-          tag: data.tag,
-        });
-      } catch (error) {
-        console.error(error);
-      }
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
     };
-    if (id) getPromptDetails();
-  }, [id]);
-
+    if (promptId) getPromptDetails();
+  }, [promptId]);
   const UpdatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
-    if (!id) return alert("Prompt ID not found");
+    if (!promptId) return alert("Prompt ID not found");
     try {
-      const response = await fetch(`/api/prompt/${id}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
+      const response = await fetch(
+        `/api/prompt/${promptId}`,
+
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            prompt: post.prompt,
+
+            tag: post.tag,
+          }),
+        }
+      );
 
       if (response.ok) {
         router.push("/");
-      } else {
-        throw new Error("Failed to update prompt");
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
     } finally {
       setSubmitting(false);
     }
   };
-
   return (
     <Form
       type="Edit"
